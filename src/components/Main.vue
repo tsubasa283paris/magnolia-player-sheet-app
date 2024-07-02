760129e45b9b1e701d9c90367ced78d91e17d2e4
tsubasa283paris <tsubasa283paris@gmail.com>
2024-07-03 04:53:29 +0900

    Add basic functionality and components

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,81 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const techPoint = ref(0);
+const faithPoint = ref(0);
+const techLevel = computed(() => {
+  return pointToLevel(techPoint.value);
+});
+const faithLevel = computed(() => {
+  return pointToLevel(faithPoint.value);
+});
+
+const pointToLevel = (point: number): number => {
+  return Math.floor(Math.log2(point + 1));
+};
+</script>
+
 <template>
-  <v-container class="fill-height">
-    <v-responsive class="align-centerfill-height mx-auto">
-      <div class="text-center">
-        <h1 class="font-weight-bold">
-          Magnolia Player Sheet
-        </h1>
-      </div>
-    </v-responsive>
-  </v-container>
+  <div min-width="400px" max-width="800px" class="fill-height d-flex align-center justify-center">
+    <v-container>
+      <v-layout row>
+        <v-col cols="6" class="text-center">
+          <v-icon icon="mdi-cog-outline" class="text-h2"></v-icon>
+        </v-col>
+        <v-col cols="6" class="text-center">
+          <v-icon icon="mdi-book-cross" class="text-h2"></v-icon>
+        </v-col>
+      </v-layout>
+      <v-layout row class="d-flex align-center justify-center mb-10">
+        <v-col cols="3">
+          <VerticalCounter
+            :count="techPoint"
+            @increment="
+              () => {
+                if (techPoint < 15) {
+                  techPoint++;
+                }
+              }
+            "
+            @decrement="
+              () => {
+                if (techPoint > 0) {
+                  techPoint--;
+                }
+              }
+            "
+          />
+        </v-col>
+        <v-col cols="3">
+          <div class="text-center">
+            <h1 class="text-h2 font-weight-bold">{{ techLevel }}</h1>
+          </div>
+        </v-col>
+        <v-col cols="3">
+          <div class="text-center">
+            <h1 class="text-h2 font-weight-bold">{{ faithLevel }}</h1>
+          </div>
+        </v-col>
+        <v-col cols="3">
+          <VerticalCounter
+            :count="faithPoint"
+            @increment="
+              () => {
+                if (faithPoint < 15) {
+                  faithPoint++;
+                }
+              }
+            "
+            @decrement="
+              () => {
+                if (faithPoint > 0) {
+                  faithPoint--;
+                }
+              }
+            "
+          />
+        </v-col>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
---
+++ src/components/VerticalCounter.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { defineEmits, defineProps } from "vue";
+
+defineProps<{
+  count: number;
+}>();
+
+const emit = defineEmits(["increment", "decrement"]);
+
+const incr = () => {
+  emit("increment", null);
+};
+const decr = () => {
+  emit("decrement", null);
+};
+</script>
+
+<template>
+  <div class="d-flex flex-column justify-center">
+    <v-btn
+      density="compact"
+      size="regular"
+      rounded="lg"
+      variant="outlined"
+      @click="incr()"
+      min-height="60px"
+    >
+      <v-icon icon="mdi-menu-up" class="text-h2 my-n4"></v-icon>
+    </v-btn>
+    <div class="text-center">
+      <h1 class="text-h3 font-weight-bold">{{ count }}</h1>
+    </div>
+    <v-btn
+      density="compact"
+      size="regular"
+      rounded="lg"
+      variant="outlined"
+      @click="decr()"
+      min-height="60px"
+    >
+      <v-icon icon="mdi-menu-down" class="text-h2 my-n4"></v-icon>
+    </v-btn>
+  </div>
+</template>
